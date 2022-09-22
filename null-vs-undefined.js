3ae3cb41889db2db3c7bf64ba3f86c498f98888b
ShaylaAnika <anikabcic@gmail.com>
2022-09-22 16:13:16 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,28 @@
+// let pakhi;
+// console.log(pakhi);
+
+// function add(num1 , num2){
+//     console.log(num1 + num2);
+//     return
+// }
+// const result = add(13, 82);
+// console.log(result);
+
+// function add(num1 , num2){
+//     console.log(num1 ,num2);
+    
+// }
+// const result = add(13);
+// console.log(result);
+
+const premik ={
+    name: 'smart dude',
+    phone: 45678,
+}
+console.log(premik.gf); 
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [12 ,34 ,13];
+console.log(ages[11]);
