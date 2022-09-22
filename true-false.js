7428f999aefd35c2f77e5c2922650a3a5bc666ec
ShaylaAnika <anikabcic@gmail.com>
2022-09-22 14:55:41 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,22 @@
+
+//falsy:
+// 0
+//""
+//undefined
+//null
+//NaN
+
+
+//truthy
+//'0' , ' ' , {},[]
+// 
+
+
+const name = 0;
+
+if(name || name == 0 ){
+    console.log('condition is true')
+}
+else{
+    console.log('condition is false')
+}
